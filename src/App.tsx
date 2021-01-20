f8f788072420c9d453d3ca4913fe399aebd65b09
최민지 <choimj0728@douzone.com>
2021-01-20 14:56:42 +0900

    11

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './router/AppRouter';
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <AppRouter />
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
